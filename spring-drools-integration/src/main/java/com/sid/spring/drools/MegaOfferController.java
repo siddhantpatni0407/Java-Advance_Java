19c8015455f068d669730ec2b44a93a1b6624ed7
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2021-04-28 12:45:59 +0530

    Spring Boot and Drools Integrtion
    Spring Boot and Drools Integrtion

---
+++ spring-drools-integration/src/main/java/com/sid/spring/drools/MegaOfferController.java
@@ -0,0 +1,20 @@
+package com.sid.spring.drools;
+
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MegaOfferController {
+	@Autowired
+	private KieSession session;
+
+	@PostMapping("/order")
+	public Order orderNow(@RequestBody Order order) {
+		session.insert(order);
+		session.fireAllRules();
+		return order;
+	}
+}
---
+++ spring-drools-integration/src/main/java/com/sid/spring/drools/SpringDroolsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.sid.spring.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDroolsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDroolsIntegrationApplication.class, args);
+	}
+
+}
