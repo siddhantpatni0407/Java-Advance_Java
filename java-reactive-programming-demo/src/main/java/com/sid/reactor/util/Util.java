bfda3c124202b95e851ed3d9ee380f94b3137c72
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2022-09-12 00:57:22 +0530

    Revert "Flux Emitting Iteam Examples"
    This reverts commit e166472454ecb91f3685f117ccdb026f086e4d66.

--- java-reactive-programming-demo/src/main/java/com/sid/reactor/util/Util.java
+++ java-reactive-programming-demo/src/main/java/com/sid/reactor/util/Util.java
@@ -1,16 +1,23 @@
 package com.sid.reactor.util;
 
 import com.github.javafaker.Faker;
-import org.reactivestreams.Subscriber;
 
 import java.util.function.Consumer;
 
 public class Util {
 
-    private static final Faker FAKER = Faker.instance();
+    public static final Faker FAKER = Faker.instance();
 
     public static Consumer<Object> onNext() {
         return o -> System.out.println("Received : " + o);
+    }
+
+    public static void sleepSeconds(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Consumer<Throwable> onError() {
@@ -23,25 +30,5 @@ public class Util {
 
     public static Faker faker() {
         return FAKER;
-    }
-
-    public static void sleepSeconds(int seconds) {
-        sleepMillis(seconds * 1000);
-    }
-
-    public static void sleepMillis(int millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static Subscriber<Object> subscriber() {
-        return new DefaultSubscriber();
-    }
-
-    public static Subscriber<Object> subscriber(String name) {
-        return new DefaultSubscriber(name);
     }
 }
