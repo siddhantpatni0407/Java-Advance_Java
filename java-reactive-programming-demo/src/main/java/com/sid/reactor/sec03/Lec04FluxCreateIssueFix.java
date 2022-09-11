e166472454ecb91f3685f117ccdb026f086e4d66
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2022-09-12 00:56:29 +0530

    Flux Emitting Iteam Examples

---
+++ java-reactive-programming-demo/src/main/java/com/sid/reactor/sec03/Lec04FluxCreateIssueFix.java
@@ -0,0 +1,29 @@
+package com.sid.reactor.sec03;
+
+import com.sid.reactor.util.Util;
+import reactor.core.publisher.Flux;
+
+/**
+ * @author Siddhant Patni
+ */
+public class Lec04FluxCreateIssueFix {
+
+    public static void main(String[] args) {
+
+        // only one instance of fluxsink
+        Flux.create(fluxSink -> {
+                    String country;
+                    int counter = 0;
+                    do {
+                        country = Util.faker().country().name();
+                        System.out.println("emitting : " + country);
+                        fluxSink.next(country);
+                        counter++;
+                    } while (!country.toLowerCase().equals("canada") && !fluxSink.isCancelled() && counter < 10);
+                    fluxSink.complete();
+                })
+                .take(3)
+                .subscribe(Util.subscriber());
+
+    }
+}
